32cc6b381a6597e6bab12466360e5717fcad5c31
gaurav <gauravtanwar2001@gmail.com>
2022-06-25 15:04:51 +0100

    Camera

--- camera.py
+++ camera.py
@@ -5,8 +5,8 @@ from sh import gphoto2 as gp
 import signal, os, subprocess
 import logging
 
-logging.basicConfig(level='Debug')
-log = logging.getLogger("Camera")
+logging.basicConfig(level='INFO')
+log = logging.getLogger("CAMERA")
 
 
 class Camera:
@@ -16,12 +16,13 @@ class Camera:
     def get_instance():
         if Camera.__instance is None:
             return Camera()
-        logging.debug("Fetching Camera Instance")
+        logging.info("Fetching Camera Instance")
         return Camera.__instance
 
     def __init__(self):
         if Camera.__instance is not None:
-            raise Exception("Camera Instance is not None")
+            logging.info("Camera Instance is not None")
+            #raise Exception("Camera Instance is not None")
         else:
             self.shot_date = None
             self.shot_time = None,
@@ -52,9 +53,9 @@ class Camera:
         try:
             os.makedirs(self.save_location)
         except OSError:
-            print("Directory Already exists")
+            logging.info("Directory Already exists")
         except:
-            print("Error Creating the directory")
+            logging.info("Error Creating the directory")
         os.chdir(self.save_location)
 
     def capture_images(self):
@@ -68,12 +69,12 @@ class Camera:
             if len(filename) < 13:
                 if filename.endswith(".JPG"):
                     os.rename(filename, (self.shot_time + ID + ".JPG"))
-                    print("Renamed the JPG")
-                    print(filename, self.shot_time + ID + ".JPG")
+                    logging.info("Renamed the JPG")
+                    return self.shot_time + ID + ".JPG"
                 elif filename.endswith(".CR2"):
                     os.rename(filename, (self.shot_time + ID + ".CR2"))
-                    print("Renamed the CR2")
-                    print(filename, self.shot_time + ID + ".CR2")
+                    logging.info("Renamed the CR2")
+                    return self.shot_time + ID + ".CR2"
 
     def run(self):
         self.kill_gphoto2_process()
@@ -81,10 +82,14 @@ class Camera:
         self.create_save_folder()
         self.capture_images()
         self.shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.rename_files(self.picID)
+        file = self.rename_files(self.picID)
+        logging.info(file)
 
 # killgphoto2Process()
 # gp(clearCommand)
 # createSaveFolder()
 # captureImages()
 # renameFiles(picID)
+if __name__ == "__main__":
+    camera = Camera.get_instance()
+    camera.run()
