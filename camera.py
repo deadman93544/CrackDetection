b01a3f4d14ca209b92250e36f4665eb5a39498f3
Prabhat Ranjan <prabhuisthename@gmail.com>
2022-06-25 18:39:36 +0530

    Camera.py

--- camera.py
+++ camera.py
@@ -1,50 +1,90 @@
+import logging
 from time import sleep
 from datetime import datetime
 from sh import gphoto2 as gp
 import signal, os, subprocess
+import logging
 
-def killgphoto2Process():
-    p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    for line in out.splitlines():
-        if b'gvfsd-gphoto2' in line:
-            pid=int(line.split(None,1)[0])
-            os.kill(pid, signal.SIGKILL)
+logging.basicConfig(level='Debug')
+log = logging.getLogger("Camera")
 
-shot_date = datetime.now().strftime("%Y-%m-%d")
-shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-picID = "PiShots"
-clearCommand = ["--folder", "/store_00020001/DCIM/100D3000/", "-R", "--delete-all-files"]
-triggerCommand = ["--trigger-capture"]
-downloadCommand = ["--get-all-files"]
-folder_name = shot_date + picID
-save_location = "/home/gaurav/Desktop/gphoto/images" + folder_name
 
-def createSaveFolder():
-    try:
-        os.makedirs(save_location)
-    except:
-        print("Failed to create the new directory.")
-    os.chdir(save_location)
+class Camera:
+    __instance = None
 
-def captureImages():
-    gp(triggerCommand)
-    sleep(3)
-    gp(downloadCommand)
-    gp(clearCommand)
+    @staticmethod
+    def get_instance():
+        if Camera.__instance is None:
+            return Camera()
+        logging.debug("Fetching Camera Instance")
+        return Camera.__instance
 
-def renameFiles(ID):
-    for filename in os.listdir("."):
-        if len(filename) < 13:
-            if filename.endswith(".JPG"):
-                os.rename(filename, (shot_time + ID + ".JPG"))
-                print("Renamed the JPG")
-            elif filename.edswith(".CR2"):
-                os.rename(filename, (shot_time + ID + ".CR2"))
-                print("Renamed the CR2")
+    def __init__(self):
+        if Camera.__instance is not None:
+            raise Exception("Camera Instance is not None")
+        else:
+            self.shot_date = None
+            self.shot_time = None,
+            self.picID = 'PiShots'
+            self.clearCommand = ["--folder", "/store_00020001/DCIM/100D3000/", "-R", "--delete-all-files"]
+            self.triggerCommand = ["--trigger-capture"]
+            self.downloadCommand = ["--get-all-files"]
+            self.save_location = "/home/gaurav/Desktop/gphoto/images"
 
-killgphoto2Process()
-gp(clearCommand)
-createSaveFolder()
-captureImages()
-renameFiles(picID)
+    def kill_gphoto2_process(self):
+        p = subprocess.Popen(['ps', '-A'], stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        for line in out.splitlines():
+            if b'gvfsd-gphoto2' in line:
+                pid = int(line.split(None, 1)[0])
+                os.kill(pid, signal.SIGKILL)
+
+    # shot_date = datetime.now().strftime("%Y-%m-%d")
+    # shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # picID = "PiShots"
+    # clearCommand = ["--folder", "/store_00020001/DCIM/100D3000/", "-R", "--delete-all-files"]
+    # triggerCommand = ["--trigger-capture"]
+    # downloadCommand = ["--get-all-files"]
+    # folder_name = shot_date + picID
+    # save_location = "/home/gaurav/Desktop/gphoto/images" + folder_name
+
+    def create_save_folder(self):
+        try:
+            os.makedirs(self.save_location)
+        except OSError:
+            print("Directory Already exists")
+        except:
+            print("Error Creating the directory")
+        os.chdir(self.save_location)
+
+    def capture_images(self):
+        gp(self.triggerCommand)
+        sleep(3)
+        gp(self.downloadCommand)
+        gp(self.clearCommand)
+
+    def rename_files(self, ID):
+        for filename in os.listdir("."):
+            if len(filename) < 13:
+                if filename.endswith(".JPG"):
+                    os.rename(filename, (self.shot_time + ID + ".JPG"))
+                    print("Renamed the JPG")
+                    print(filename, self.shot_time + ID + ".JPG")
+                elif filename.endswith(".CR2"):
+                    os.rename(filename, (self.shot_time + ID + ".CR2"))
+                    print("Renamed the CR2")
+                    print(filename, self.shot_time + ID + ".CR2")
+
+    def run(self):
+        self.kill_gphoto2_process()
+        gp(self.clearCommand)
+        self.create_save_folder()
+        self.capture_images()
+        self.shot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.rename_files(self.picID)
+
+# killgphoto2Process()
+# gp(clearCommand)
+# createSaveFolder()
+# captureImages()
+# renameFiles(picID)
